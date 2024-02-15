2e5eb3a2ff01351b969eac43287c92d81c777fad
Islam Diab <islamdiab304@gmail.com>
2024-02-15 09:28:27 +0200

    etse

---
+++ lib/core/theming/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles {
+  static TextStyle font24Red = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.bold,
+    color: Colors.red,
+  );
+  static TextStyle font16Red = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.bold,
+    color: Colors.red,
+  );
+}
